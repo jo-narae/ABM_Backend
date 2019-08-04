57587a972cc8e1fd0b12a7008fe750f2a13d9a94
이명학 <leemh920725@gmail.com>
2019-08-04 19:38:47 +0900

    환경구축-명학

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ const router = new Router();
 const api = require('./api');
 
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+router.get('/', (ctx, next) => {
+    ctx.body = '홈';
+});
 
+//app.use(router.routers());
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4500, () => {
